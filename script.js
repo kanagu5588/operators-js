8ccc8e4eaa4e5c4867ed064c295d77877cb2b2cf
kanagu5588 <120088469+kanagu5588@users.noreply.github.com>
2023-03-30 12:30:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+function add()
+{
+    let value1=Number(document.getElementById("submit1").value);
+    let value2=Number(document.getElementById("submit2").value);
+    let result=value1+value2;
+    alert(result);
+}
+
+function sub()
+{
+    let value1=Number(document.getElementById("submit1").value);
+    let value2=Number(document.getElementById("submit2").value);
+    let result=value1-value2;
+    alert(result);
+}
+function mult()
+{
+    let value1=Number(document.getElementById("submit1").value);
+    let value2=Number(document.getElementById("submit2").value);
+    let result=value1*value2;
+    alert(result);
+}
+function div()
+{
+    let value1=Number(document.getElementById("submit1").value);
+    let value2=Number(document.getElementById("submit2").value);
+    let result=value1/value2;
+    alert(result);
+}
+function mod()
+{
+    let value1=Number(document.getElementById("submit1").value);
+    let value2=Number(document.getElementById("submit2").value);
+    let result=value1%value2;
+    alert(result);
+}
